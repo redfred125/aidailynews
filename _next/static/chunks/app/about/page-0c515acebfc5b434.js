850b5f973bbfd0d97f86ba9c9dc9942c8870623f
Jeff <jeff@aidailynews.org>
2026-02-08 18:05:02 +0000

    Fix routing for new pages and rebuild

---
+++ _next/static/chunks/app/about/page-0c515acebfc5b434.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301,467,779,888,922,339,327,906,219,385,127,571,128,785,484],{2699:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=2699)}),_N_E=n.O()}]);
--- dist/_next/static/chunks/app/guides/securing-ai-agent/page-64ee207a05d9ee6a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[219,301,467,888,922,779,339,128,906,785,327,127,484,385,571],{2699:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=2699)}),_N_E=n.O()}]);
